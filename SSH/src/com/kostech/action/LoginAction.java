8334769b2e54042099a27d3b925ba3584fff36c6
zlj <zlj@192.168.5.1>
2016-01-28 13:32:33 +0800

    ssh

---
+++ SSH/src/com/kostech/action/LoginAction.java
@@ -0,0 +1,14 @@
+package com.kostech.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		String result = "";
+		return result;
+	}
+}
