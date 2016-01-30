bd4bd82a3c27c8d9b7275f4e038132f8cec49114
zlj <zlj@192.168.5.1>
2016-01-30 14:44:58 +0800

    hibernate mapping...

---
+++ SSH/src/com/kostech/beans/Grade.java
@@ -0,0 +1,46 @@
+package com.kostech.beans;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Grade implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer gid;
+	private String gname;
+	private String desc ;
+	private Set<Student> stus=new HashSet<Student>();
+	@Override
+	public String toString() {
+		return "Grade [gid=" + gid + ", gname=" + gname + ", desc=" + desc + ", stus=" + stus + "]";
+	}
+	public Integer getGid() {
+		return gid;
+	}
+	public void setGid(Integer gid) {
+		this.gid = gid;
+	}
+	public String getGname() {
+		return gname;
+	}
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Set<Student> getStus() {
+		return stus;
+	}
+	public void setStus(Set<Student> stus) {
+		this.stus = stus;
+	}
+}
---
+++ SSH/src/com/kostech/beans/Student.java
@@ -0,0 +1,39 @@
+package com.kostech.beans;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sex=" + sex + "]";
+	}
+
+	private Integer sid;
+	private String sname;
+	private String  sex;
+}
